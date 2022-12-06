9e5b2eb9912f38f9067e0858bd3b9b46dd5874e4
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2022-12-06 10:58:39 -0300

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3bbba2009fb6a3da"; }
+/******/ 		__webpack_require__.h = function() { return "bd7c546f0e77ab41"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/faq.js
+++ pages/faq.js
@@ -1,36 +1,52 @@
-import { useEffect, useState } from 'react';
+import Head from 'next/head'
 import Link from '../src/components/Link';
 
+// SSG - Static Site Generation
+// SSR - Server Side Rendering
+// ISG - Incremental Static Generation
 
-export default function FAQPage() {
+// export async function getServerSideProps() {
+//     console.log('Em modo DEV, sempre roda! A cada acesso')
+//     console.log('Rodando a cada acesso que você recebe')
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
 
-    const[faq,setFaq] = useState([]);
+    return {
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
 
-    useEffect(() => {
-        const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
-        fetch(FAQ_API_URL)
-            .then((respostaDoServidor) => {
-                return respostaDoServidor.json();
-            })
-            .then((resposta) => {
-                setFaq(resposta);
-            })
-    }, []);
-
+export default function FAQPage({ faq }) {
+    console.log('Isso roda no servidor???')
     return (
         <div>
+            <Head>
+                <title>FAQ - Alura Cases Campanha</title>
+            </Head>
             <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
             <Link href="/">
                 Ir para a home
             </Link>
             <ul>
-                {faq.map(({answer, question}) =>(
-                <li key={question} >
-                    <article>
-                        <h2>{question}</h2>
-                        <p>{answer}</p>
-                    </article>
-                </li>
+                {faq.map(({ answer, question }) => (
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
                 ))}
             </ul>
         </div>
